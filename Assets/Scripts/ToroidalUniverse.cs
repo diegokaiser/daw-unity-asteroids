7ac3e52317d10473fb3c362013765c16ac207de6
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2022-10-21 12:21:18 +0200

    toroidal for player

--- Assets/Scripts/ToroidalUniverse.cs
+++ Assets/Scripts/ToroidalUniverse.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+
+public struct Borders
+{
+    public float superiorBorder, inferiorBorder, leftBorder, rightBorder;
+}
+
 public class ToroidalUniverse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public Borders borders;
+
     void Update()
     {
-        
+        var pos = transform.position;
+        var x = transform.position.x;
+        var y = transform.position.y;
+
+        if (x > borders.rightBorder)
+        {
+            pos.x = borders.leftBorder;
+            transform.position = pos;
+        }
+        if (x < borders.leftBorder)
+        {
+            pos.x = borders.rightBorder;
+            transform.position = pos;
+        }
+        if (y > borders.superiorBorder)
+        {
+            pos.y = borders.inferiorBorder;
+            transform.position = pos;
+        }
+        if (y < borders.inferiorBorder)
+        {
+            pos.y = borders.superiorBorder;
+            transform.position = pos;
+        }
     }
 }
